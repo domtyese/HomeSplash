89c181b8e875bed4d15ab1f894741c23b1671fd7
Dominique Riley <dtr9323@gmail.com>
2015-03-02 20:35:44 -0600

    got the clock and greeting working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
-  end
+  	@current_time = Time.now.to_formatted_s (:short)
+  	@greet_message = case Time.now.hour
+  		when 5..11
+  		"Good Morning!"
+  		when 12..15
+  		"Good Afternoon!"
+  		when 16..19
+  		"Good Evening!"
+  		when 20..23, 0..4
+  		"Good Night!"
+  		end
+
+	end
+
 end
